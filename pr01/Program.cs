9950589a0ef039bc85da89fb18c3143f9002f2a7
Grebennik <alexander.grebennik@netgroup.com>
2024-12-02 14:14:59 +0200

    day 1

--- pr01/Program.cs
+++ pr01/Program.cs
@@ -1,40 +1,20 @@
 ﻿var lines = File.ReadAllLines("TextFile1.txt");
-var result = Second(lines);
-Console.WriteLine(result);
 
-int Second(string[] lines)
+var left = new List<int>();
+var right = new List<int>();
+
+foreach (var line in lines)
 {
-    var numbers = new[]
-    {
-        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-    }.Union("1234567890".Select(c => "" + c)).ToArray();
-
-    var result = lines
-        .Select(line =>
-        {
-            var first = numbers.Select(x => new { Index = line.IndexOf(x), Number = x })
-                .Where(x => x.Index > -1)
-                .OrderBy(x => x.Index).Select(x => x.Number).First();
-            var last = numbers.Select(x => new { Index = line.LastIndexOf(x), Number = x })
-                .Where(x => x.Index > -1)
-                .OrderBy(x => x.Index).Select(x => x.Number).Last();
-            if (first.Length > 1)
-                first = (Array.IndexOf(numbers, first) + 1).ToString();
-            if (last.Length > 1)
-                last = (Array.IndexOf(numbers, last) + 1).ToString();
-            return first + last;
-        })
-        .Select(x => int.Parse(x))
-        .Sum();
-    return result;
+    var splits = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+    left.Add(int.Parse(splits[0]));
+    right.Add(int.Parse(splits[1]));
 }
+left.Sort();
+right.Sort();
 
-int First(string[] lines)
-{
-    var digits = "1234567890";
-    var result = lines
-        .Select(x => "" + x.First(c => digits.Contains(c)) + x.Last(c => digits.Contains(c)))
-        .Select(x => int.Parse(x))
-        .Sum();
-    return result;
-}
+Console.WriteLine(First(left, right));
+Console.WriteLine(Second(left, right));
+
+int Second(List<int> left, List<int> right) => left.Sum(x => x * right.Count(y => y == x));
+
+int First(List<int> left, List<int> right) => left.Zip(right).Sum(x => Math.Abs(x.First - x.Second));

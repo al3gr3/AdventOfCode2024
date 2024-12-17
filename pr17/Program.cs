0bf9dde9a73cdea60cb6154d522426faf86099f5
al3gr3 <alexander.grebennik@gmail.com>
2024-12-17 12:24:27 +0200

    day 17 pt 2

--- pr17/Program.cs
+++ pr17/Program.cs
@@ -1,73 +1,118 @@
 ﻿var lines = File.ReadAllLines("TextFile1.txt");
-var regA = int.Parse(lines[0].Split(' ').Last());
-var regB = int.Parse(lines[1].Split(' ').Last());
-var regC = int.Parse(lines[2].Split(' ').Last());
+var regA = decimal.Parse(lines[0].Split(' ').Last());
+var regB = decimal.Parse(lines[1].Split(' ').Last());
+var regC = decimal.Parse(lines[2].Split(' ').Last());
+List<int> output = new List<int>();
+var instructions = new[] { adv, bxl, bst, jnz, bxc, @out, bdv, cdv, };
+var program = lines[4].Split(' ').Last().Split(',').Select(int.Parse).ToArray();
+
 var instructionPointer = 0;
-List<string> output = new List<string>();
 
-var instructions = new Action<int>[]
+Console.WriteLine(Go(regA));
+Console.WriteLine(Second());
+
+long Second()
 {
-    adv, bxl, bst, jnz, bxc, @out, bdv, cdv,
-};
+    var programStr = string.Join(',', program);
+    var queue = new Queue<long>();
+    queue.Enqueue(0);
+    while (queue.Any())
+    {
+        var next = queue.Dequeue();
+        for (int i = 8 * 8 * 8; i < 8 * 8 * 8 * 8; i++)
+        {
+            var p = i + next * 8 * 8 * 8 * 8;
+            var s = Go(p);
+            if (programStr == s)
+                return p;
 
-var program = lines.Last().Split(' ').Last().Split(',').Select(int.Parse).ToArray();
-
-while (instructionPointer < program.Length)
-{
-    var instructionIndex = program[instructionPointer];
-    instructions[instructionIndex](program[instructionPointer + 1]);
-    if (!(instructionIndex == 3 && regA != 0))
-        instructionPointer += 2;
+            if (programStr.EndsWith(s))
+                queue.Enqueue(p);
+        }
+    }
+    throw new Exception();
 }
 
-Console.WriteLine(string.Join(',', output));
-foreach (var x in new[] { regA, regB, regC })
-{ 
-    Console.WriteLine(x); 
-}   
+string  Go(decimal a)
+{
+    regA = a;
+    regB = 0;
+    regC = 0;
+    output.Clear();
+    instructionPointer = 0;
+    while (instructionPointer < program.Length)
+    {
+        var instructionIndex = program[instructionPointer];
+        instructions[instructionIndex](program[instructionPointer + 1]);
+        instructionPointer += 2;
+    }
 
+    var res = string.Join(',', output);
+    return res;
+}
+
+decimal Combo(int operand) => new[] { 0, 1, 2, 3, regA, regB, regC }[operand];
+decimal DivRegA(int operand) => (long) (regA / (int)Math.Pow(2, (double)Combo(operand)));
+
+// 2,4,    1,1,    7,5,    4,0,      0,3,     1,6,     5,5,     3,0
+
+// regB = (regA % 8) ^ 1                    // 2,4 1,1  
+// regB ^= regA / (2 ** regB)               // 7,5 4,0
+// regA /= 8                                // 0,3
+// regB ^= 6                                // 1,6
+// output regB % 8                          // 5,5
+// if (regA != 0) goto start                // 3,0
+
+// regB = (regA % 8) ^ 7 ^ (regA / (2 ** ((regA % 8) ^ 1)))
+
+// 0
 void adv(int operand)
 {
-    regA /= (int)Math.Pow(2, Combo(operand)); 
+    regA = DivRegA(operand);
 }
 
+// 1
 void bxl(int operand)
 {
-    regB ^= operand;
+    regB = (long)regB ^ operand;
 }
 
+// 2
 void bst(int operand)
 {
-    var val = Combo(operand);
-    regB = val % 8;
+    regB = Combo(operand) % 8;
 }
 
-int Combo(int operand) => operand < 4 ? operand : new[] { regA, regB, regC }[operand - 4];
-
+// 3
 void jnz(int operand)
 {
-    if (regA == 0)
-        return;
-
-    instructionPointer = operand;
+    if (regA != 0)
+        instructionPointer = operand - 2;
 }
 
+// 4
 void bxc(int _)
 {
-    regB ^= regC;
+    regB = (long)regB ^ (long)regC;
 }
 
+// 5
 void @out(int operand)
 {
-    output.Add((Combo(operand) % 8).ToString());
+    output.Add((int)(Combo(operand) % 8));
 }
 
+// 6
 void bdv(int operand)
 {
-    regB = regA / (int)Math.Pow(2, Combo(operand));
+    regB = DivRegA(operand);
 }
 
+// 7
 void cdv(int operand)
 {
-    regC = regA / (int)Math.Pow(2, Combo(operand));
+    regC = DivRegA(operand);
 }
+
+
+decimal Aggr(int[] regABytes) => regABytes.Reverse().Aggregate((decimal)0, (s, n) => s * 8 + n);

d10894cb4209da7be62e7b159045e65b71ea59b4
al3gr3 <alexander.grebennik@gmail.com>
2024-12-17 15:44:52 +0200

    day 16 pt 2

--- pr16/Program.cs
+++ pr16/Program.cs
@@ -2,14 +2,34 @@
 var height = lines.Length;
 var width = lines.First().Length;
 
-//var prev = Enumerable.Range(1, height).Select(x => new Point[width]).ToArray();
+var prev = new Dictionary<string, List<Position>>();
+
 var dist = Enumerable.Range(1, height).Select(x => Enumerable.Range(1, width).Select(c => new[] { int.MaxValue, int.MaxValue, int.MaxValue, int.MaxValue }).ToArray()).ToArray();
 var queue = Enumerable.Range(0, height).SelectMany(y => Enumerable.Range(0, width)
     .SelectMany(x => new[] { 0, 1, 2, 3}.Select(d => new Position { Dir = d, P = new Point { X = x, Y = y } }))).ToList();
 
-First();
+Dijkstra();
 
-void First()
+var result = dist[1][width - 2].Min();
+Console.WriteLine(result);
+
+var back = new Queue<Position>();
+var all = new List<Position>();
+
+new[] { 0, 1, 2, 3 }.Where(x => dist[1][width - 2][x] == result).ToList().ForEach(d => back.Enqueue(new Position { P = new Point { X = width - 2, Y = 1 }, Dir = d }));
+
+while (back.Any())
+{
+    var b = back.Dequeue();
+    all.Add(b);
+    if (prev.ContainsKey(b.Represent))
+        foreach (var p in prev[b.Represent])
+            if (!all.Any(z => z.IsEqual(p)))
+                back.Enqueue(p);
+}
+Console.WriteLine(all.GroupBy(p => $"{p.P.X}|{p.P.Y}").Count());
+
+void Dijkstra()
 {
     dist[height - 2][1][1] = 0;
 
@@ -20,45 +40,50 @@ void First()
         if (dist[u.P.Y][u.P.X][u.Dir] == int.MaxValue)
             continue;
 
-        var newPos = new Position { P = u.P.AddClone(Point.OrtoDirections[u.Dir]), Dir = u.Dir };
-
-        if (lines[newPos.P.Y][newPos.P.X] != '#')
+        var newPoses = new[]
         {
-            var v = queue.FirstOrDefault(x => x.P.X == newPos.P.X && x.P.Y == newPos.P.Y && x.Dir == newPos.Dir);
-            if (v != null)
+            new Position { P = u.P.AddClone(Point.OrtoDirections[u.Dir]), Dir = u.Dir },
+            new Position { P = u.P.Clone(), Dir = (u.Dir + -1 + 4) % 4 },
+            new Position { P = u.P.Clone(), Dir = (u.Dir +  1 + 4) % 4 },
+        };
+
+        for (int i = 0; i < newPoses.Length; i++)
+        {
+            var newPos = newPoses[i];
+            if (lines[newPos.P.Y][newPos.P.X] != '#')
             {
-                var alt = dist[u.P.Y][u.P.X][u.Dir] + 1;
-                if (alt < dist[v.P.Y][v.P.X][v.Dir])
+                var v = queue.FirstOrDefault(x => x.P.X == newPos.P.X && x.P.Y == newPos.P.Y && x.Dir == newPos.Dir);
+                if (v != null)
                 {
-                    dist[v.P.Y][v.P.X][v.Dir] = alt;
-                    //prev[v.Y][v.X] = u.Clone();
+                    var alt = dist[u.P.Y][u.P.X][u.Dir] + (i == 0 ? 1 : 1000);
+
+                    if (alt == dist[v.P.Y][v.P.X][v.Dir])
+                    {
+                        var key = v.Represent;
+                        if (prev.ContainsKey(key))
+                            prev[key].Add(u);
+                        else
+                            prev[key] = new[] { u }.ToList();
+                    }
+                    else if (alt < dist[v.P.Y][v.P.X][v.Dir])
+                    {
+                        dist[v.P.Y][v.P.X][v.Dir] = alt;
+                        prev[v.Represent] = new[] { u }.ToList();
+                    }
                 }
             }
         }
-
-        new[] { -1, 1 }.Select(lr => (u.Dir + lr + 4) % 4 ).ToList().ForEach(directionIndex =>
-        {
-            var newPos = new Position { P = u.P.Clone(), Dir = directionIndex };
-
-            var v = queue.FirstOrDefault(x => x.P.X == newPos.P.X && x.P.Y == newPos.P.Y && x.Dir == newPos.Dir);
-            if (v != null)
-            {
-                var alt = dist[u.P.Y][u.P.X][u.Dir] + 1000;
-                if (alt < dist[v.P.Y][v.P.X][v.Dir])
-                {
-                    dist[v.P.Y][v.P.X][v.Dir] = alt;
-                    //prev[v.Y][v.X] = u.Clone();
-                }
-            }
-        });
     }
-    Console.WriteLine(dist[2][width - 2].Min() + 1); // 109506 too high
 }
 
 class Position
 {
     internal Point P;
     internal int Dir;
+
+    internal string Represent => $"{P.X}|{P.Y}|{Dir}";
+
+    internal bool IsEqual(Position p) => this.Dir == p.Dir && P.IsEqual(p.P);
 }
 
 class Point

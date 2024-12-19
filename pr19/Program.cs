cb221cdae4f366ab513c6c51b9cb3055dc5efbec
al3gr3 <alexander.grebennik@gmail.com>
2024-12-19 09:21:36 +0200

    day 19

---
+++ pr19/Program.cs
@@ -0,0 +1,13 @@
+﻿var lines = File.ReadAllLines("TextFile1.txt");
+var towels = lines.First().Split(new[] { ", "}, StringSplitOptions.RemoveEmptyEntries);
+var todos = lines.Skip(2).ToList();
+
+var dict = new Dictionary<string, long>() { { "", 1 } };
+
+Console.WriteLine(todos.Count(x => CountWays(x) > 0));
+Console.WriteLine(todos.Sum(CountWays));
+
+long CountWays(string arg) => towels.Where(x => arg.StartsWith(x)).Select(x => arg[x.Length..])
+    .Sum(next => dict.ContainsKey(next)
+        ? dict[next]
+        : dict[next] = CountWays(next));
